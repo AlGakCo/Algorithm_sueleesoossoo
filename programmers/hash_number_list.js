88f4334fadc46de38b3740b3b144809b94dd260f
leesoo7595 <isoo7510@gmail.com>
2020-05-03 17:31:39 +0900

    :sparkles: solution 2: hash number list

--- programmers/hash_number_list.js
+++ programmers/hash_number_list.js
@@ -5,11 +5,38 @@ function solution(arr) {
     for (let filteredEle of filteredArr) {
       splitEle = filteredEle.slice(0, ele.length)
       if (splitEle.includes(ele)) {
-        return true;
+        return false
       }
     }
   }
-  return false;    
+  return true   
+}
+// const startTime = new Date().getTime()
+// console.log(solution(['357', '23', '83575', '753', '12345', '785']))
+// const endTime = new Date().getTime()
+// console.log('1. ', endTime - startTime)
+
+function solution2(arr) {
+  // const startTime = performance.now()
+  const mapArr = new Set(arr.map(e => e.length))
+  let sliceArr
+  for (let ele of mapArr) {
+      sliceArr = new Set(arr.map(e => {
+        if (e.length < ele) return `null${e}`
+        return e.slice(0, ele)
+      }))
+      if (sliceArr.size !== arr.length) {
+        return false
+      }
+  }
+  // const endTime = performance.now()
+  // console.log('2. ', endTime - startTime)
+  return true
 }
 
-console.log(solution(['357', '23', '83575', '753', '12345', '35785']))
+// console.log(solution2(['357', '23', '83575', '753', '12345', '85']))
+
+const startTime = new Date().getTime()
+console.log(solution2(['357', '23', '83575', '753', '12345', '785']))
+const endTime = new Date().getTime()
+console.log('1. ', endTime - startTime)
