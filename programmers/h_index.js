20c375c1a45333c92f3920a90146dc08d5a75a50
leesoo7595 <isoo7510@gmail.com>
2020-05-17 20:32:46 +0900

    :sparkles: h-index

---
+++ programmers/h_index.js
@@ -0,0 +1,27 @@
+function solution(citations) {
+  const obj = {};
+  const sortedArr = citations.sort((a, b) => a - b)
+  for (let el of sortedArr) {
+      if (obj[el]) {
+          obj[el].push(el);
+      } else {
+          obj[el] = [el]
+          Object.keys(obj).forEach(e => {
+              if (e < el) {
+                  obj[e].push(el);
+              }
+          })
+      }
+  }
+  console.log('obj', obj);
+  const arr = [];
+  Object.keys(obj).forEach(e => {
+      if (obj[e].length >= e) {
+          arr.push(e*1);
+      }
+  })
+  console.log('arr', arr);
+  return Math.max(...arr)
+}
+
+console.log(solution([10, 50, 100]))
