a2351ff420e08aaa3037483a1708146728d3e65f
leesoo7595 <isoo7510@gmail.com>
2020-05-31 18:54:10 +0900

    :sparkles: bfs decimal

---
+++ programmers/decimal.js
@@ -0,0 +1,22 @@
+function solution(numbers) {
+  const arr = Array.prototype.map.call(numbers, e => e*1)
+  console.log(arr, numbers.length)
+  let returnable = 0
+  arr.forEach(element => {
+    if (isPrime(element)) returnable++
+  });
+  console.log(returnable)
+  return returnable
+}
+
+function isPrime(num) {
+ 
+  for (i = 2; i < num; i++) {
+      if (num % i === 0) {
+          return false;
+      }
+  }
+  return true;
+}
+
+solution("17")
